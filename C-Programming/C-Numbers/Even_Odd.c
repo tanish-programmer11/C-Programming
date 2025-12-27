5344b543c9ef0a13197ddba27ac4ceea0596f029
tanish-programmer11 <c.chauhan250001@gmail.com>
2025-12-27 13:41:58 +0530

    Even_Odd.c

--- C-Programming/C-Numbers/Even_Odd.c
+++ C-Programming/C-Numbers/Even_Odd.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
-int main() {
-    int n;
-     printf("Enter a number:");
-    scanf("%d",&n);
-    if(n%2==0) 
-       printf("Even");
-    else 
-       printf("Odd");
-    return 0;
-}   
+#include <stdio.h>   // Header file for input-output functions like printf and scanf
+
+int main() {          // Main function where program execution starts
+    int n;            // Variable to store the input number
+
+    printf("Enter a number:");   // Ask the user to enter a number
+    scanf("%d", &n);             // Read the number entered by the user
+
+    // Check whether the number is even or odd
+    if (n % 2 == 0)              // If remainder is 0 when divided by 2
+        printf("Even");          // Then the number is even
+    else                         // Otherwise
+        printf("Odd");           // The number is odd
+
+    return 0;                    // End of program
+}
