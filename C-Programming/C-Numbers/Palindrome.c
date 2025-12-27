63f5d842ce303efbad060f52acc4547da0e4ddde
tanish-programmer11 <c.chauhan250001@gmail.com>
2025-12-27 10:01:27 +0530

    Palindrome.c

---
+++ C-Programming/C-Numbers/Palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main (){
+ char str[100];
+  int len,i;
+   printf("enter number");
+   scanf("%s",str);
+  len= strlen(str);
+  for(i=0;i<len/2;i++){
+    if(str[i]!=str[len-1-i]){
+     printf("not palindrome" );
+  return 0;
+}
+}
+ printf ("NOt palindrome" );
+ 
+ return 0;
+ }
