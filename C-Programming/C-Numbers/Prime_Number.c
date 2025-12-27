4daf16feaa523b6b8b534ff8dde12895c74c2d78
tanish-programmer11 <c.chauhan250001@gmail.com>
2025-12-27 13:46:21 +0530

    Prime_Number.c

--- C-Programming/C-Numbers/Prime_Number.c
+++ C-Programming/C-Numbers/Prime_Number.c
@@ -21,3 +21,28 @@ int main () {
 
     return 0;            // End program
 }
+
+
+
+ 
+
+
+
+#include <stdio.h>
+#include <string.h>
+int main (){
+ char str[100];
+  int len,i;
+   printf("enter number");
+   scanf("%s",str);
+  len= strlen(str);
+  for(i=0;i<len/2;i++){
+    if(str[i]!=str[len-1-i]){
+     printf("not palindrome" );
+  return 0;
+}
+}
+ printf ("Palindrome" );
+
+ return 0;
+ }
