83b0c2f056a982f02b1ade2e0414abf152e61bba
tanish-programmer11 <c.chauhan250001@gmail.com>
2025-12-27 13:42:46 +0530

    Palindrome.c

--- C-Programming/C-Numbers/Palindrome.c
+++ C-Programming/C-Numbers/Palindrome.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-int main (){
- char str[100];
-  int len,i;
-   printf("enter number");
-   scanf("%s",str);
-  len= strlen(str);
-  for(i=0;i<len/2;i++){
-    if(str[i]!=str[len-1-i]){
-     printf("not palindrome" );
-  return 0;
+#include <stdio.h>    // Header file for input-output functions
+#include <string.h>   // Header file for string handling functions like strlen
+
+int main () {
+    char str[100];    // Array to store the input number as a string
+    int len, i;       // Variables for length of string and loop counter
+
+    printf("enter number");  
+    scanf("%s", str);        // Take number input in string format
+
+    len = strlen(str);       // Find the length of the input string
+
+    // Loop to check characters from start and end
+    for (i = 0; i < len / 2; i++) {
+        // Compare characters from beginning and end
+        if (str[i] != str[len - 1 - i]) {
+            printf("not palindrome");  // If mismatch found
+            return 0;                  // Exit the program
+        }
+    }
+
+    printf("Palindrome");   // If no mismatch found, it is a palindrome
+
+    return 0;               // End of program
 }
-}
- printf ("Palindrome" );
- 
- return 0;
- }
